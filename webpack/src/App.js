c96cbe29e4b62f812ddf89a02ea9187a0f239aed
fubei <relaxto@qq.com>
2017-03-07 00:44:18 +0800

    package_install

---
+++ webpack/src/App.js
@@ -0,0 +1,7 @@
+/**
+ * Created by chengfubei on 2017/3/7 0007.
+ */
+import React from "react"
+export default function App() {
+    return <h1>hello cheng!</h1>
+}
---
+++ webpack/src/client.js
@@ -0,0 +1,9 @@
+/**
+ * Created by chengfubei on 2017/3/7 0007.
+ */
+
+import React from "react";
+import {render} from "react-dom";
+import App from "./App";
+
+render((<App/>),document.querySelector("#example"))
